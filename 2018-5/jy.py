6a26e64f5b5857893eec493228871f6226bc92fe
haunghaikong <649344475@qq.com>
2018-06-20 14:02:09 +0800

    文华财经程序打开，自动刷新，自动读取数据

--- 2018-5/jy.py
+++ 2018-5/jy.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 class HS:
     def __init__(self):
-        self.SXF = {"HSIK8": 33.54, "MHIK8": 13.6}  # 13.6
-        self.hy = {"HSIK8": 50, "MHIK8": 10}  # 10
+        self.SXF = {"HSI": 33.54, "MHI": 13.6}  # 13.6
+        self.hy = {"HSI": 50, "MHI": 10}  # 10
 
     def get_data(self, file_name):
         dd = []
@@ -98,7 +98,7 @@ class HS:
             if data['jy'] != 0:
                 data['cb'] = data['all_price'] / data['jy']
                 # jcbs = self.SXF * 2 / 50 * data['jy']
-                jcbs = (data['wcds1'] + abs(data['jy'])) * self.SXF[msg[3]] * 2 / self.hy[msg[3]] / data['jy']
+                jcbs = (data['wcds1'] + abs(data['jy'])) * self.SXF[msg[3][:3]] * 2 / self.hy[msg[3][:3]] / data['jy']
                 # sum_cb = sum([cb[1] if cb[0]>0 else -cb[1] for cb in data['all_kp']]) # 净成本
                 sum_cb = data['cb'] + jcbs  # (sum_cb-jcbs)/data['jy']
                 data['jcb'] = int(sum_cb) if data['jy'] < 0 else (
@@ -106,12 +106,12 @@ class HS:
 
             data['time'] = msg[2]
 
-    def ray(self, df):
+    def transfer(self, df):
         """ df:
-                 bs    price                 time       code
-            0    -1   30934.0   2018/05/11 09:15:26     HSIK8
-            1    1   30941.0   2018/05/11 09:15:29      HSIK8
-            """
+                         bs    price                 time       code
+                    0    -1   30934.0   2018/05/11 09:15:26     HSIK8
+                    1    1   30941.0   2018/05/11 09:15:29      HSIK8
+                    """
         res = []
         ind = 0
         is_ind = -1
@@ -136,23 +136,36 @@ class HS:
             pjyl = round(data['all'] / data['wcds'], 2) if data['wcds'] > 0 else 0  # 平均盈利
             huihuapj = round(data['pcyl_all'] / data['wcds1'], 2) if data['wcds1'] > 0 else 0  # 会话平均盈利
             zcb = round(data['sum_price'] / data['jy'], 2) if data['jy'] != 0 else round(data['sum_price'], 2)  # 持仓成本
-            jzcbs = (data['wcds'] + abs(data['jy'])) * self.SXF[msg[3]] * 2 / self.hy[msg[3]] / data['jy'] if data[
-                                                                                                                  'jy'] != 0 else 0  # self.SXF * 2 / 50 * data['jy']
+            jzcbs = (data['wcds'] + abs(data['jy'])) * self.SXF[msg[3][:3]] * 2 / self.hy[msg[3][:3]] / data['jy'] if \
+            data[
+                'jy'] != 0 else 0  # self.SXF * 2 / 50 * data['jy']
             jzcb = (data['sum_price'] / data['jy'] + jzcbs) if data['jy'] != 0 else 0  # 净持仓成本
             jzcb = int(jzcb) + 1 if jzcb > int(jzcb) else int(jzcb)
 
-            jlr = round(data['all'] * self.hy[msg[3]] - self.SXF[msg[3]] * data['all_jy_add'], 2)  # 净利润
+            jlr = round(data['all'] * self.hy[msg[3][:3]] - self.SXF[msg[3][:3]] * data['all_jy_add'], 2)  # 净利润
             jpjlr = round(jlr / data['wcds'], 2) if data['wcds'] > 0 else 0  # 净平均利润
 
             if ind != is_ind or (ind == is_ind and data['jy'] == 0):
                 res.append(
                     [msg[3], data['time'], msg[0], pri, data['jy'], yscb, cb, jcb, cbyl, data['pcyl_all'], data['all'],
                      pjyl,
-                     huihuapj, zcb, jzcb, data['all'] * self.hy[msg[3]], jlr, jpjlr,
-                     round(self.SXF[msg[3]] * data['all_jy_add'], 2), data['wcds'], data['dbs']])
+                     huihuapj, zcb, jzcb, data['all'] * self.hy[msg[3][:3]], jlr, jpjlr,
+                     round(self.SXF[msg[3][:3]] * data['all_jy_add'], 2), data['wcds'], data['dbs']])
                 cbyl = 0
             data['dbs'] += 1 if data['jy'] == 0 else 0  # 序号
             is_ind = ind
+        return res
+
+    def ray(self, df):
+        res = []
+        codes = set(df.iloc[:, 3])
+        if len(codes) == 1:
+            res = self.transfer(df)
+        else:
+            for code in codes:
+                df2 = df[df.iloc[:, 3] == code]
+                rs = self.transfer(df2)
+                res += rs
 
         columns = ['合约', '时间', '开仓', '当前价', '持仓', '原始成本', '会话成本', '净会话成本', '此笔盈利', '会话盈利', '总盈利', '总平均盈利', '会话平均盈利',
                    '持仓成本', '净持仓成本', '利润', '净利润', '净平均利润', '手续费', '已平仓', '序号']
@@ -162,7 +175,7 @@ class HS:
 
 if __name__ == "__main__":
     h = HS()
-    dd = h.get_data(r'D:\tools\Tools\May_2018\2018-5-7\2018May25a.txt')  # 2018May2.txt  2018May11.txt
+    dd = h.get_data(r'D:\tools\Tools\June_2018\2018-6-8\2018May25.txt')  # 2018May2.txt  2018May11.txt
     res = h.ray(dd)
     print(res)
     import os
