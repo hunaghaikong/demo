006592268631c7503ecc68e7c0eb830557939102
haunghaikong <649344475@qq.com>
2018-12-20 10:54:30 +0800

    工具

--- AndBefore_2018_3/MyUtil.py
+++ AndBefore_2018_3/MyUtil.py
@@ -1,7 +1,10 @@
 
-import pymysql
 import json
 import re
+import pymongo
+import datetime
+import math
+import pymysql
 import configparser
 
 config=configparser.ConfigParser()
@@ -13,6 +16,122 @@ def get_conn(dataName):
 def closeConn(conn):
     conn.commit()
     conn.close()
+
+
+
+def dtf(d):
+    """ 日期时间格式化 """
+    if isinstance(d, str):
+        d = d.strip()
+        if len(d) == 10:
+            d = datetime.datetime.strptime(d, '%Y-%m-%d')
+        else:
+            d = datetime.datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
+        return d
+    elif isinstance(d, datetime.datetime):
+        d = datetime.datetime.strftime(d, '%Y-%m-%d %H:%M:%S')
+        return d
+    elif isinstance(d, datetime.date):
+        d = datetime.datetime.strftime(d, '%Y-%m-%d')
+        return d
+
+
+class MongoDBData:
+    """ MongoDB 数据库的连接与数据查询处理类 """
+
+    def __init__(self, db=None, table=None):
+        self.db_name = db
+        self.table = table
+        # if not self._coll:
+        #     self._coll = self.get_coll()
+        self._coll = self.get_coll()
+
+    def get_coll(self):
+        client = pymongo.MongoClient('mongodb://192.168.2.226:27017')
+        client.admin.authenticate(config['MONGODB']['us'], config['MONGODB']['ps'])
+        self.db = client[self.db_name] if self.db_name else client['Future']
+        coll = self.db[self.table] if self.table else self.db['future_1min']
+        return coll
+
+    def get_hsi(self, sd, ed, code='HSI'):
+        """
+        获取指定开始日期，结束日期，指定合约的恒指分钟数据
+        :param sd: 开始日期
+        :param ed: 结束日期
+        :param code: 合约代码
+        :return:
+        """
+
+        if isinstance(sd, str):
+            sd = dtf(sd)
+        if isinstance(ed, str):
+            ed = dtf(ed)
+        dates = set()
+        start_dates = [sd]
+        _month = sd.month
+        _year = sd.year
+        e_y = ed.year
+        e_m = ed.month
+        _while = 0
+
+        while _year < e_y or (_year == e_y and _month <= e_m):
+            _month = sd.month + _while
+            _year = sd.year + math.ceil(_month / 12) - 1
+            _month = _month % 12 if _month % 12 else 12
+            code = code[:3] + str(_year)[2:] + ('0' + str(_month) if _month < 10 else str(_month))
+            try:
+                _ed = self.db['future_contract_info'].find({'CODE': code})[0]['EXPIRY_DATE']
+            except:
+                return
+            if _ed not in start_dates:
+                start_dates.append(_ed)
+            _while += 1
+            if sd >= _ed:
+                continue
+            _sd = start_dates[-2]
+
+            if _sd > ed:
+                return
+            data = self._coll.find({'datetime': {'$gte': _sd, '$lt': _ed}, 'code': code},
+                                   projection=['datetime', 'open', 'high', 'low', 'close', 'volume']).sort('datetime',
+                                                                                                           1)  # 'HSI1808'
+
+            _frist = True
+            for i in data:
+                date = i['datetime']
+                if _frist:
+                    _frist = False
+                    exclude_time = str(date)[:10]
+                    dates.add(datetime.datetime.strptime(exclude_time + ' 09:14:00', '%Y-%m-%d %H:%M:%S'))
+                    dates.add(datetime.datetime.strptime(exclude_time + ' 12:59:00', '%Y-%m-%d %H:%M:%S'))
+                if date not in dates:
+                    dates.add(date)
+                    if date > ed:
+                        return
+                    yield [date, i['open'], i['high'], i['low'], i['close'], i['volume']]
+
+    def data_day(self, code, date):
+        """ 获取一天的期货数据 """
+        day = datetime.timedelta(days=1)
+        data = self._coll.find({'datetime': {'$gte': date, '$lt': date + day}, 'code': code},
+                               projection=['datetime', 'open', 'high', 'low', 'close', 'trade']).sort('datetime', 1)
+        # 时间，开盘，收盘，最低，最高，成交量
+        # data = [[i['datetime'], i['open'], i['close'], i['low'], i['high'], i['trade']] for i in data]
+        # data.sort()
+        # return data
+        for i in data:
+            yield [i['datetime'], i['open'], i['close'], i['low'], i['high'], i['trade']], i['datetime'].hour
+
+
+def sql_data(st='2018-11-08', ed='2018-11-10'):
+    sql = f"SELECT DATETIME,OPEN,high,low,CLOSE,vol FROM wh_same_month_min WHERE prodcode='HSI' AND DATETIME>='{st}' AND DATETIME<='{ed}' ORDER BY DATETIME"
+    conn = pymysql.connect(db='carry_investment', user=config['U']['us'], passwd=config['U']['ps'], host='192.168.2.226',charset='utf8')
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.close()
+    data = cur.fetchall()
+    for i in data:
+        yield i
 
 class MyUtil:
     def __init__(self):
